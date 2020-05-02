888fa79144d9684c392ec340afab7ea69d5d984a
Nigel Fernandes <fernandes.nigelj@gmail.com>
2020-05-01 23:14:35 -0400

    init server

---
+++ api/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const PORT = 3001;
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => res.send("Hello world"));
+
+app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
