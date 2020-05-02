9a51aa4e70370b09dbd3998b56bdd0e6689ac0fc
Nigel Fernandes <fernandes.nigelj@gmail.com>
2020-05-02 17:43:32 -0400

    add redux + router

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import STORE from './store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={STORE}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ client/src/store/index.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+import reducers from '../reducers';
+
+const middlewares = [thunk];
+
+const store = createStore(
+    reducers,
+    applyMiddleware(...middlewares)
+);
+
+export default store;
