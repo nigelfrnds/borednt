6fec026a7c0b77394c55aa43e458a743bb17234f
Nigel Fernandes <fernandes.nigelj@gmail.com>
2020-05-03 16:58:11 -0400

    add youtube API + controllers + utils

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 
 const movieRoutes = require('./routes/movies');
 const tvShowRoutes = require('./routes/tv-shows');
+const videoRoutes = require('./routes/videos');
 
 const PORT = 3001;
 
@@ -13,5 +14,6 @@ app.use(bodyParser.json());
 
 app.use('/api/movies', movieRoutes);
 app.use('/api/tv-shows', tvShowRoutes);
+app.use('/api/videos', videoRoutes);
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
