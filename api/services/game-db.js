5f83913c9c62f9d3709a412191fb68d86b0ff613
Saad Iqbal <s1iqbal@ryerson.ca>
2020-05-03 15:46:58 -0400

    removed declaration outside of function scope

--- api/services/game-db.js
+++ api/services/game-db.js
@@ -1,23 +1,21 @@
 const axios = require('axios');
 const { cacheResult } = require('../services/redis');
-
+const GAME_HOST_URL = process.env.GAME_HOST_URL,
+      GAME_HOST_KEY = process.env.GAME_HOST_URL,
+      baseUrl = 'https://rawg-video-games-database.p.rapidapi.com/games';
 
 const fetchPopularGames = async (cacheKey) => {
-    const GAME_HOST_URL = process.env.GAME_HOST_URL,
-          GAME_HOST_KEY = process.env.GAME_HOST_URL;
-
     try {
-        const baseUrl = 'https://rawg-video-games-database.p.rapidapi.com/games';
+        const 
         const apiRequest = await axios({
-            "method":"GET",
-            "url": baseUrl,
-            "headers":{
-            "content-type":"application/octet-stream",
-            "x-rapidapi-host":GAME_HOST_URL,
-            "x-rapidapi-key":GAME_HOST_KEY
+                "method":"GET",
+                "url": baseUrl,
+                "headers":{
+                "content-type":"application/octet-stream",
+                "x-rapidapi-host":GAME_HOST_URL,
+                "x-rapidapi-key":GAME_HOST_KEY
             }
-            });
-        console.log(apiRequest);
+        });
         const data = apiRequest.data;
         const results = data.results;
 
