c2b36e7a5642ba5b2969a18eee857cb9572d94ef
Nigel Fernandes <fernandes.nigelj@gmail.com>
2020-05-14 14:25:23 -0400

    clean up deployment code + add start container script

---
+++ deploy/start.sh
@@ -0,0 +1,19 @@
+!/bin/bash
+
+echo "Starting server..."
+
+activeContainers=`docker ps | wc -l`
+
+if [ $activeContainers -gt 1 ]
+then
+    echo "Shutting down active containers..."
+    docker-compose -f deployment.yml down
+    echo "Restarting containers with updated images..."
+    docker-compose -f deployment.yml up -d --build
+else
+    echo "No containers running. Starting containers..."
+    docker-compose -f deployment.yml up -d --build
+fi
+
+echo "Containers started succesfully"
+    
